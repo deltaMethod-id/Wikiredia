c029b849f2117edff36af585b52433461d529662
Meta <wahyudede930@gmail.com>
2026-08-18 03:39:15 +0700

    Add Loading component for organizations

---
+++ app/organizations/loading.tsx
@@ -0,0 +1,9 @@
+export default function Loading() {
+  return (
+    <main className="page-container">
+      <div className="loading-state">
+        Loading organizations...
+      </div>
+    </main>
+  );
+}
